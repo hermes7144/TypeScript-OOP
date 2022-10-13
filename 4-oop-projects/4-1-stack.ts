94378f3be62f5bd579ef71d9a777519cf0b0f8d0
KCCHO <kichancho@gmail.com>
2022-10-13 21:30:13 +0900

    스택 TS, OOP로 구현

---
+++ 4-oop-projects/4-1-stack.ts
@@ -0,0 +1,41 @@
+interface Stack {
+  readonly size: number;
+  push(value: string): void;
+  pop(): string;
+}
+
+type StackNode = {
+  value: string;
+  next?: StackNode;
+};
+
+class StackImpl implements Stack {
+  private _size: number = 0;
+  private head?: StackNode;
+  get size(): number {
+    return this._size;
+  }
+  push(value: string) {
+    const node: StackNode = { value, next: this.head };
+    this.head = node;
+    this._size++;
+  }
+  pop(): string {
+    if (this.head == null) {
+      throw new Error('Stack is empty!');
+    }
+    const node = this.head;
+    this.head = node.next;
+    this._size--;
+    return node.value;
+  }
+}
+
+const stack = new StackImpl();
+stack.push('Ellie 1');
+stack.push('Ellies 2');
+stack.push('Elliel 3');
+
+while (stack.size !== 0) {
+  console.log(stack.pop());
+}
